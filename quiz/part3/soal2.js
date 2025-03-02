54616e15f7d69c31da5f3fc170a96e7ba07d0761
AchmadRendang <akupro1232@gmail.com>
2025-03-02 11:53:36 +0700

    soal 2 part3 done

--- quiz/part3/soal2.js
+++ quiz/part3/soal2.js
@@ -1,21 +1,19 @@
 // Problem
-// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. 
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
 // Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
 
-let number = 0;
+
 const patokanMaju = 5;
 const patokanMundur = 0;
 
 console.log("LOOPING MAJU");
-while (number < patokanMaju){
-    number++;
-    console.log(number);
+for(let i = 0 ; i < patokanMaju; i++){
+    console.log(i);
 }
 
 console.log("LOOPING MUNDUR");
-while(number > patokanMundur){
-    number--;
-    console.log(number);
+for(let x = 5; x>patokanMundur; x--){
+    console.log(x);
 }
